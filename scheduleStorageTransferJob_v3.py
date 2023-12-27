93c56994c545ff5b5c4a8e3bfbd8f8f2a2098423
darpan_raj_meesho <darpan_raj_meesho>
2023-12-27 11:24:11 +0530

    ds dbs ofs

--- scheduleStorageTransferJob_v3.py
+++ scheduleStorageTransferJob_v3.py
@@ -102,17 +102,17 @@ if __name__== '__main__':
         timestamp_pb = Timestamp()
         timestamp_pb.FromDatetime(current_time)
 
-        future_time = current_time + timedelta(minutes=30)
+        future_time = current_time + timedelta(days=15)
         future_timestamp_pb = Timestamp()
         future_timestamp_pb.FromDatetime(future_time)
 # 10AM everyday 
         start_seconds = timestamp_pb.seconds % 60
-        # end_seconds = future_timestamp_pb.seconds % 60
+        end_seconds = future_timestamp_pb.seconds % 60
 
         schedule_dict = {
             "schedule_start_date": {"year": current_time.year, "month": current_time.month, "day": current_time.day},
             "start_time_of_day": {"hours": current_time.hour, "minutes": current_time.minute, "seconds": start_seconds},
-            # "end_time_of_day": {"hours": future_time.hour, "minutes": future_time.minute, "seconds": end_seconds},
+            "end_time_of_day": {"hours": future_time.hour, "minutes": future_time.minute, "seconds": end_seconds},
             "schedule_end_date": {"year": future_time.year, "month": future_time.month, "day": future_time.day},
         }
 
