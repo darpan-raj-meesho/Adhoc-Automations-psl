dfec068f3f7554cf20d36c519fd9615cb51b1d74
darpan_raj_meesho <darpan_raj_meesho>
2023-12-28 17:25:55 +0530

    singapore

--- scheduleStorageTransferJob_v3.py
+++ scheduleStorageTransferJob_v3.py
@@ -130,7 +130,7 @@ if __name__== '__main__':
                 # path = 'singapore-prod/3660535150785951/',
                 # endpoint=f"{endpoint1 if switchEndpoint else endpoint2 }",
                 endpoint=f"{endpoint1}",
-                region='ap-southeast-1',
+                region='us-west-2',
                 s3_metadata=storage_transfer_v1.S3CompatibleMetadata(
                     auth_method=storage_transfer_v1.S3CompatibleMetadata.AuthMethod.AUTH_METHOD_AWS_SIGNATURE_V4,
                     request_model=storage_transfer_v1.S3CompatibleMetadata.RequestModel.REQUEST_MODEL_VIRTUAL_HOSTED_STYLE,
